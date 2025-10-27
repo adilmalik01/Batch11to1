b3e6eb0890f4ebd3b5dc779ffcc2517a47aebba6
adilmalik01 <malikadil5679@gmail.com>
2025-10-27 12:14:19 +0500

    user input &  if else (completed)

---
+++ Javascript/5.user_input/app.js
@@ -0,0 +1,14 @@
+
+
+
+
+// let firstName = prompt("Enter your first name")
+// let lastName = prompt("Enter your last name")
+// let age = prompt("Enter your Age")
+
+
+// console.log( "Full Name : " + firstName , lastName , "Age" + age )
+
+
+// console.log(`Full Name : ${firstName} ${lastName} 
+// Age : ${age}   `)
---
+++ Javascript/6.if_else/app.js
@@ -0,0 +1,44 @@
+
+
+// let verify = 5 == 6
+// // console.log(verify)
+
+
+// if (verify) {
+
+//     console.log("5 is equal to 5")
+
+// } else {
+
+//     console.log("5 is not equal to 5")
+
+// }
+
+
+
+
+let emailDb = prompt("Enter your email !")
+let passwordDb = prompt("Enter your password")
+
+
+
+// console.log(emailDb == "adil@gmail.com" && passwordDb == 1234)
+
+
+let validation = emailDb == "adil@gmail.com" && passwordDb == 1234
+
+if(validation){
+    console.log("User Login")
+}else{
+    console.log("Invalid Email Or Password")
+}
+
+
+
+
+
+
+
+
+
+
