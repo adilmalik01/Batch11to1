2c42414a3a29e76bf377766b70a1585205ec3707
adilmalik01 <malikadil5679@gmail.com>
2025-10-29 12:39:55 +0500

    2nd method

--- Javascript/7.calculator/app.js
+++ Javascript/7.calculator/app.js
@@ -2,14 +2,19 @@
 
 
 let first_number = parseInt( prompt("Enter your first number :") )
-let operator = prompt("Enter your Operator :")
+
+let operator = prompt(`Choose your operator number :
+1) Add
+2) Subtract
+3) Multiply `)
+
 let second_number = +prompt("Enter your second number :")
 
-if (operator == "+") {
+if (operator == "1") {
     alert(first_number + second_number)
-}else if(operator == "-"){
+}else if(operator == "2"){
    alert(first_number - second_number)
-}else if(operator == "*"){
+}else if(operator == "3"){
     alert(first_number * second_number)
 }else{
     alert("Invalid Operator")
