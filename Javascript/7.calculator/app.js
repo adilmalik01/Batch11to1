8cabc9fff39a8d63abd00a4fee3b97580742ed5b
adilmalik01 <malikadil5679@gmail.com>
2025-10-29 11:52:59 +0500

    calculator complete

---
+++ Javascript/7.calculator/app.js
@@ -0,0 +1,33 @@
+
+
+
+let first_number = parseInt( prompt("Enter your first number :") )
+let operator = prompt("Enter your Operator :")
+let second_number = +prompt("Enter your second number :")
+
+if (operator == "+") {
+    alert(first_number + second_number)
+}else if(operator == "-"){
+   alert(first_number - second_number)
+}else if(operator == "*"){
+    alert(first_number * second_number)
+}else{
+    alert("Invalid Operator")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
