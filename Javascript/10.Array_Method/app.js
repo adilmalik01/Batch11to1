537673bd5a0d3be5a518563f748b726ec00c80ac
adilmalik01 <malikadil5679@gmail.com>
2025-11-10 12:27:13 +0500

    Array & Array methods

---
+++ Javascript/10.Array_Method/app.js
@@ -0,0 +1,9 @@
+// let names = ["Adil","Aqib","Hamza",56,true]
+// console.log(names) 
+// console.log(names[3]) 
+
+// let name = "aqib" , ""
+
+let students = ["Aqib","Ali","Hamza","Azan","Adil","Hello"]
+
+// console.log(students)
---
+++ Javascript/9.Array/app.js
@@ -0,0 +1,40 @@
+// push 
+// pop
+// shift
+// unshift
+// slice
+// splice
+
+
+
+
+let students = ["Aqib","Ali","Hamza","Azan","Adil","Hello"]
+
+// students.push("Azan","Adil")   Appends new elements to the end of an array,
+// console.log(students)
+
+// console.log(students)  Removes the last element from an array and returns it
+// students.pop()
+// console.log(students)
+
+
+
+
+// students.shift()
+// console.log(students)
+
+
+// students.unshift("Hello")
+// console.log(students);
+
+
+
+// let copyArray = students.slice(2,5)
+// console.log(copyArray);
+// console.log(students);
+
+
+
+console.log(students)
+students.splice(2,1,"World")
+console.log(students)
