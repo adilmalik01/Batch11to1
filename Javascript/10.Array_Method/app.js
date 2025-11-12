b63a1901546083a77350d7cafd116a0fb804d972
adilmalik01 <malikadil5679@gmail.com>
2025-11-12 11:55:06 +0500

    update : file replace issue

--- Javascript/10.Array_Method/app.js
+++ Javascript/10.Array_Method/app.js
@@ -1,9 +1,42 @@
-// let names = ["Adil","Aqib","Hamza",56,true]
-// console.log(names) 
-// console.log(names[3]) 
 
-// let name = "aqib" , ""
+
+// push 
+// pop
+// shift
+// unshift
+// slice
+// splice
+
+
+
 
 let students = ["Aqib","Ali","Hamza","Azan","Adil","Hello"]
 
+// students.push("Azan","Adil")   Appends new elements to the end of an array,
 // console.log(students)
+
+// console.log(students)  Removes the last element from an array and returns it
+// students.pop()
+// console.log(students)
+
+
+
+
+// students.shift()
+// console.log(students)
+
+
+// students.unshift("Hello")
+// console.log(students);
+
+
+
+// let copyArray = students.slice(2,5)
+// console.log(copyArray);
+// console.log(students);
+
+
+
+console.log(students)
+students.splice(2,1,"World")
+console.log(students)
