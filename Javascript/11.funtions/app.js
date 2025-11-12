cffa223507bede922770cdb0fc15db0d45aaaffd
adilmalik01 <malikadil5679@gmail.com>
2025-11-12 12:18:22 +0500

    funtions

---
+++ Javascript/11.funtions/app.js
@@ -0,0 +1,50 @@
+// let num1 = 5
+// let num2 = 8
+// let sum = num1 + num2
+// console.log(sum)
+
+
+
+function printLogic(num1,num2,num3) {
+  
+   
+    let sum = num1 + num2  + num3
+    console.log(sum)
+
+
+}
+
+
+
+
+
+
+
+
+
+
+printLogic(5,5,4)
+printLogic(5,8,9)
+printLogic(5,10,4)
+
+
+
+
+
+// printLogic()
+// printLogic()
+// printLogic()
+// printLogic()
+// printLogic()
+// printLogic()
+
+
+
+
+
+
+
+
+
+
+
