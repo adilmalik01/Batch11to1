92f58b6583bc99e0c9a571dbc9a7e997f837b35d
adilmalik01 <malikadil5679@gmail.com>
2025-11-17 13:08:05 +0500

    functions complete

--- Javascript/11.funtions/app.js
+++ Javascript/11.funtions/app.js
@@ -1,3 +1,8 @@
+
+
+
+
+
 // let num1 = 5
 // let num2 = 8
 // let sum = num1 + num2
@@ -5,44 +10,49 @@
 
 
 
-function printLogic(num1,num2,num3) {
-  
-   
-    let sum = num1 + num2  + num3
-    console.log(sum)
+// function printLogic(num1,num2) {
 
+
+//     let sum = num1 + num2
+//     console.log(sum)
+
+// }
+
+
+
+// printLogic(10,10)
+// printLogic(5,10)
+// printLogic(25,10)
+
+
+
+
+
+
+
+
+// function simpleFunction(){
+
+
+// }
+
+
+const printName = (Studentname, Studentage) => {
+
+    let studentObject = {
+        name: Studentname,
+        age: Studentage
+    }
+
+    console.log(studentObject)
+    console.log(studentObject.age)
 
 }
 
 
 
-
-
-
-
-
-
-
-printLogic(5,5,4)
-printLogic(5,8,9)
-printLogic(5,10,4)
-
-
-
-
-
-// printLogic()
-// printLogic()
-// printLogic()
-// printLogic()
-// printLogic()
-// printLogic()
-
-
-
-
-
-
+printName("Adil", 20)
+printName("Ali", 30)
 
 
 
