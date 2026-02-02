a9140b98dfebaacac50e2ba0bbf1d47c7982795c
adilmalik01 <malikadil5679@gmail.com>
2026-02-02 11:52:28 +0500

    for loop complete

---
+++ Javascript/15.loops/app.js
@@ -0,0 +1,60 @@
+
+// For loop 
+
+let students = ["Adil", "Ali", "Hamza", "Aqib"]
+
+// console.log(students[0]);
+// console.log(students[1]);
+// console.log(students[2]);
+// console.log(students[3]);
+
+for (let i = 0; i < students.length; i++) {
+    console.log(students[i]);
+}
+
+
+
+
+
+
+// ES6
+// FOREACH
+// MAP
+// FOR OF
+// FOR IN
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
