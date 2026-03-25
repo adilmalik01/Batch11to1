7cc87c1d2f8e3132527cb5247975a949778c5331
Muhammad Adil <malikadil5679@gmail.com>
2026-03-25 12:22:21 +0500

    update: php

---
+++ PHP/index.php
@@ -0,0 +1,77 @@
+<?php  
+
+// $my_name = "Adil";
+// $age = 123;
+// $is_Active = false;
+
+// echo $my_name;
+// echo $age;
+// echo $is_Active;
+
+
+
+// $num1 = 10;
+// $num2 = 10;
+
+// $sum = $num1 * $num2;
+// echo $sum;
+
+
+// $num1 = 10;
+// $num2 = 30;
+
+// $sum = $num1 + $num2;
+
+// $sum += 10;
+
+// echo $sum;
+
+
+
+
+
+
+//  Comparision Operators
+// == < >  <=  >=  !
+
+// echo 5 < 5;
+
+// echo "<br>";
+
+// echo 5 > 5;
+// echo "<br>";
+
+// echo 5 <= 5;
+// echo "<br>";
+
+// echo 5 >= 5;
+// echo "<br>";
+
+// echo 5 == 5;
+// echo "<br>";
+
+// echo 5 != 5;
+
+
+
+$email  = "adil@gmail.com";
+$password = "12345678";
+
+$verify = $email == "adil@gmail.com" && $password == "12345678" ? "Login Success" : "Login Failed";
+
+
+echo $verify;
+// if($verify){
+//     echo "Login Success";  
+// }else{
+//     echo "Login Failed";
+// }
+
+
+
+
+
+
+
+
+?>
